04c19dc202bee69c8443e905b785f8e0e52655bb
Ali Tariq <thechampwarrior200@outlook.com>
2023-01-10 02:59:19 -0500

    Animation Added

--- TingDog/script.js
+++ TingDog/script.js
@@ -1,7 +1,18 @@
 const loadingContainer = document.querySelector('.loading-container')
 const websiteContainer = document.querySelector('.main-website')
+const allSections = document.querySelectorAll('.section')
 
 setTimeout(() => {
     loadingContainer.classList.add('hidden')
     websiteContainer.classList.remove('hidden')
 },5000)
+
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle('section--show',entry.isIntersecting)
+       
+    })
+})
+
+allSections.forEach(section => observer.observe(section))
